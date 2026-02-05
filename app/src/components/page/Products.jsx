de9a49cfde39f56121b060ffdfc07c228893f7f5
rolinagy0513 <nagyroli0513@gmail.com>
2026-02-05 10:41:44 +0100

    Removed the rounded images and removed the line between the Header and the Navbar.

--- app/src/components/page/Products.jsx
+++ app/src/components/page/Products.jsx
@@ -14,6 +14,7 @@ import translations from "../../utility/Trsanslations.js";
 
 import "./styles/Products.css"
 
+
 const Products = () =>{
 
     const {selectedLanguage} = useContext(LanguageContext);
